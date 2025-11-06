bfdb615dce4ac6515f76de66107325106ac27703
justinroame <justin_roame@yahoo.com>
2025-11-06 13:06:06 -0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,7 @@
-'use client';
-
-import { useEffect } from 'react';
-import { useRouter } from 'next/navigation';
+import RemoveBGPage from './remove/page';
 
 export const dynamic = 'force-dynamic';
 
 export default function Home() {
-  const router = useRouter();
-
-  useEffect(() => {
-    router.push('/remove');
-  }, [router]);
-
-  return (
-    <div className="flex items-center justify-center min-h-screen">
-      <p className="text-lg">Loading your AI tool...</p>
-    </div>
-  );
+  return <RemoveBGPage />;
 }
