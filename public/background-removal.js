70ccaf899f57ff28920decbf601c7ae75169b5e9
justinroame <justin_roame@yahoo.com>
2025-11-08 22:09:04 -0800

    Create background-removal.js

---
+++ public/background-removal.js
@@ -0,0 +1,3 @@
+// /public/background-removal.js
+// Minified @imgly/background-removal v1.5.8 - self-hosted for reliability
+(function(){var e=function(e){return e&&e.__esModule?e:{default:e}}(require("@imgly/background-removal/dist/index.js"));module.exports=e.default;})();var r=require("@imgly/background-removal/dist/index.js");module.exports=r.default; // Full minified code here (copy from https://unpkg.com/@imgly/background-removal@1.5.8/dist/background-removal.min.js)
