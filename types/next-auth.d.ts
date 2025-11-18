78dab1869b475916ef9b611315fe6ac80d0f6fb8
justinroame <justin_roame@yahoo.com>
2025-11-18 20:17:05 +0000

    fix: extend NextAuth types (session, JWT, User) + cleanup auth

--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -1,22 +1,28 @@
-import NextAuth, { DefaultSession } from "next-auth";
+import NextAuth, { DefaultSession, DefaultUser } from "next-auth";
 
 declare module "next-auth" {
   interface Session {
     user: {
       id: string;
+      email: string;
       totalCredits: number;
+      pro: boolean;
     } & DefaultSession["user"];
   }
 
-  interface User {
-    id: string;
+  interface User extends DefaultUser {
+    id: string | number;
+    email: string;
     totalCredits: number;
+    pro: boolean;
   }
 }
 
 declare module "next-auth/jwt" {
   interface JWT {
-    id: string;
+    id: string | number;
+    email: string;
     totalCredits: number;
+    pro: boolean;
   }
 }
