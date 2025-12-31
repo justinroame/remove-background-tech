80f85acda9a1c6362e060c11474d4b99a42ce299
justinroame <justin_roame@yahoo.com>
2025-12-31 08:11:14 +0000

    Fix Stripe API version type mismatch on pricing page

--- app/pricing/page.tsx
+++ app/pricing/page.tsx
@@ -1,9 +1,7 @@
 import Stripe from "stripe";
 import PricingInner from "./pricing-inner";
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: "2023-10-16",
-});
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
 
 export default async function PricingPage() {
   if (!process.env.STRIPE_SECRET_KEY) {
